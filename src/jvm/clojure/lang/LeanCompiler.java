de816b5fdbe10acdd69851e14d824bc304ff8dd4
Alexander Yakushev <alex@bytopia.org>
2014-07-25 01:18:18 +0300

    Move alterVarRoot special case to static method compilation

--- src/jvm/clojure/lang/LeanCompiler.java
+++ src/jvm/clojure/lang/LeanCompiler.java
@@ -3658,6 +3658,12 @@ static class InvokeExpr implements Expr{
 		if(isDirect && RT.booleanCast(EMIT_LEAN_CODE.deref()))
 			{
 			staticExpr.emit(context, objx, gen);
+			if (isAlterVarRoot)
+				{
+				Var var = ((TheVarExpr)RT.first(args)).var;
+				String typeStr = "L"+munge(var.ns.name.toString()).replace(".", "/")+"__init;";
+				gen.putStatic(Type.getType(typeStr), munge(var.sym.name), OBJECT_TYPE);
+				}
 			return;
 			}
 
@@ -3672,12 +3678,6 @@ static class InvokeExpr implements Expr{
 			fexpr.emit(C.EXPRESSION, objx, gen);
 			gen.checkCast(IFN_TYPE);
 			emitArgsAndCall(0, context,objx,gen);
-                        if (isAlterVarRoot) {
-                                Var var = ((TheVarExpr)RT.first(args)).var;
-                                String typeStr = "L"+munge(var.ns.name.toString()).replace(".", "/")+"__init;";
-                                gen.putStatic(Type.getType(typeStr), munge(var.sym.name), OBJECT_TYPE);
-                                context = C.RETURN;
-                        }
 			}
 		if(context == C.STATEMENT)
 			gen.pop();		
