e053762af5bfe07eb456c5cbb9f90a531b1f0e22
Reid 'arrdem' McKenzie <reid@factual.com>
2015-07-30 22:49:07 -0700

    Inline more

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -88,7 +88,9 @@
     :doc "Returns a seq of the items after the first. Calls seq on its
   argument.  If there are no more items, returns nil."
     :added "1.0"
-    :static true}  
+    :static true
+    :inline (fn [xe]
+              (rt (list 'next xe)))}  
   next
   (fn [x]
     (. clojure.lang.RT (next x))))
@@ -99,7 +101,9 @@
     :doc "Returns a possibly empty seq of the items after the first. Calls seq on its
   argument."
     :added "1.0"
-    :static true}  
+    :static true
+    :inline (fn [xe]
+              (rt (list 'more xe)))}  
   rest
   (fn [x]
     (. clojure.lang.RT (more x))))
