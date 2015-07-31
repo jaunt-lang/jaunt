c54d9d93178a24b3620a46732b2864f4080db3b4
Alan Malloy <alan@malloys.org>
2015-07-30 21:23:46 -0700

    Add a strict version of str

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -565,14 +565,32 @@
    :static true}
   (^String [] "")
   (^String [^Object x]
-   (if (nil? x) "" (. x (toString))))
+           (if (nil? x) "" (. x (toString))))
   (^String [x & ys]
-     ((fn [^StringBuilder sb more]
-          (if more
-            (recur (. sb  (append (str (first more)))) (next more))
-            (str sb)))
-      (new StringBuilder (str x)) ys)))
+           ((fn [^StringBuilder sb more]
+              (loop [^StringBuilder sb sb, more more]
+                (if more
+                  (recur (. sb (append (str (first more)))) (next more))
+                  (str sb))))
+            (new StringBuilder (str x)) ys)))
 
+(defn ^:strict str'
+  "With no args, returns the empty string. With one arg x, returns
+  x.toString().  (str nil) returns the empty string. With more than
+  one arg, returns the concatenation of the str values of the args."
+  {:tag String
+   :added "1.0"
+   :static true}
+  (^String [] "")
+  (^String [^Object x]
+           (if (nil? x) "" (. x (toString))))
+  (^String [x & ys]
+           ((fn [^StringBuilder sb more]
+              (loop [sb sb more more]
+                (if more
+                  (recur (. sb  (append (str (first more)))) (next more))
+                  (str sb))))
+            (new StringBuilder (str x)) ys)))
 
 (defn symbol?
   "Return true if x is a Symbol"
