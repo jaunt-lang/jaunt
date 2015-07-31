3f9a3814ce04074af27c8588ea80b1988cf73afc
Alan Malloy <alan@malloys.org>
2015-07-31 11:09:31 -0700

    Remove str' from core

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -574,24 +574,6 @@
                   (str sb))))
             (new StringBuilder (str x)) ys)))
 
-(defn ^:strict str'
-  "With no args, returns the empty string. With one arg x, returns
-  x.toString().  (str nil) returns the empty string. With more than
-  one arg, returns the concatenation of the str values of the args."
-  {:tag String
-   :added "1.0"
-   :static true}
-  (^String [] "")
-  (^String [^Object x]
-           (if (nil? x) "" (. x (toString))))
-  (^String [x & ys]
-           ((fn [^StringBuilder sb more]
-              (loop [sb sb more more]
-                (if more
-                  (recur (. sb  (append (str (first more)))) (next more))
-                  (str sb))))
-            (new StringBuilder (str x)) ys)))
-
 (defn symbol?
   "Return true if x is a Symbol"
   {:added "1.0"
