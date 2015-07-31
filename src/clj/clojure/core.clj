3419ede8094d0f0359b6acef26db2a9cd2586c07
Reid 'arrdem' McKenzie <reid@factual.com>
2015-07-30 23:49:13 -0700

    Whitespace

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -599,7 +599,8 @@
   {:tag Boolean,
    :added "1.0"
    :static true}
-  [x] (clojure.lang.Util/identical x false))
+  [x]
+  (clojure.lang.Util/identical x false))
 
 (defn true?
   "Returns true if x is the value true, false otherwise."
