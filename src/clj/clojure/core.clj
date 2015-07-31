d962f08d11b0589d9c893cb963b80fde06599ed5
Reid 'arrdem' McKenzie <reid@factual.com>
2015-07-30 22:24:24 -0700

    Anonomize next

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -80,13 +80,15 @@
     (. clojure.lang.RT (first coll))))
 
 (def
- ^{:arglists '([coll])
-   :tag clojure.lang.ISeq
-   :doc "Returns a seq of the items after the first. Calls seq on its
+  ^{:arglists '([coll])
+    :tag clojure.lang.ISeq
+    :doc "Returns a seq of the items after the first. Calls seq on its
   argument.  If there are no more items, returns nil."
-   :added "1.0"
-   :static true}  
- next (fn ^:static next [x] (. clojure.lang.RT (next x))))
+    :added "1.0"
+    :static true}  
+  next
+  (fn [x]
+    (. clojure.lang.RT (next x))))
 
 (def
  ^{:arglists '([coll])
