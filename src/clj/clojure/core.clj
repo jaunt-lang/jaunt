b94d98d2e1f8eac37071c8325a72c9ee5a67c244
Reid 'arrdem' McKenzie <reid@factual.com>
2015-07-30 22:25:20 -0700

    Anonomize conj

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -102,20 +102,24 @@
     (. clojure.lang.RT (more x))))
 
 (def
- ^{:arglists '([coll x] [coll x & xs])
-   :doc "conj[oin]. Returns a new collection with the xs
+  ^{:arglists '([coll x] [coll x & xs])
+    :doc "conj[oin]. Returns a new collection with the xs
     'added'. (conj nil item) returns (item).  The 'addition' may
     happen at different 'places' depending on the concrete type."
-   :added "1.0"
-   :static true}
- conj (fn ^:static conj
-        ([] [])
-        ([coll] coll)
-        ([coll x] (. clojure.lang.RT (conj coll x)))
-        ([coll x & xs]
-         (if xs
-           (recur (conj coll x) (first xs) (next xs))
-           (conj coll x)))))
+    :added "1.0"
+    :static true}
+  conj
+  (fn
+    ([] [])
+    ([coll] coll)
+    ([coll x]
+     (. clojure.lang.RT (conj coll x)))
+    ([coll x & xs]
+     (if xs
+       (recur (. clojure.lang.RT (conj coll x))
+              (first xs)
+              (next xs))
+       (. clojure.lang.RT (conj coll x))))))
 
 (def
  ^{:doc "Same as (first (next x))"
