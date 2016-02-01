9c56fdd78202b494e802b1cbd85c25c998312c79
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-01-31 18:43:23 -0600

    Warn when referring from ^:deprecated ns

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -4117,25 +4117,27 @@
   clashes. Use :use in the ns macro in preference to calling this directly."
   {:added "1.0"}
   [ns-sym & filters]
-    (let [ns (or (find-ns ns-sym) (throw (new Exception (str "No namespace: " ns-sym))))
-          fs (apply hash-map filters)
-          nspublics (ns-publics ns)
-          rename (or (:rename fs) {})
-          exclude (set (:exclude fs))
-          to-do (if (= :all (:refer fs))
-                  (keys nspublics)
-                  (or (:refer fs) (:only fs) (keys nspublics)))]
-      (when (and to-do (not (instance? clojure.lang.Sequential to-do)))
-        (throw (new Exception ":only/:refer value must be a sequential collection of symbols")))
-      (doseq [sym to-do]
-        (when-not (exclude sym)
-          (let [v (nspublics sym)]
-            (when-not v
-              (throw (new java.lang.IllegalAccessError
-                          (if (get (ns-interns ns) sym)
-                            (str sym " is not public")
-                            (str sym " does not exist")))))
-            (. *ns* (refer (or (rename sym) sym) v)))))))
+  (let [ns        (or (find-ns ns-sym) (throw (new Exception (str "No namespace: " ns-sym))))
+        fs        (apply hash-map filters)
+        nspublics (ns-publics ns)
+        rename    (or (:rename fs) {})
+        exclude   (set (:exclude fs))
+        to-do     (if (= :all (:refer fs))
+                    (keys nspublics)
+                    (or (:refer fs) (:only fs) (keys nspublics)))]
+    (when (and to-do (not (instance? clojure.lang.Sequential to-do)))
+      (throw (new Exception ":only/:refer value must be a sequential collection of symbols")))
+    (when (deprecated? ns)
+      (.write *err* (str "Warning: referring vars from deprecated ns: " (ns-name ns))))
+    (doseq [sym to-do]
+      (when-not (exclude sym)
+        (let [v (nspublics sym)]
+          (when-not v
+            (throw (new java.lang.IllegalAccessError
+                        (if (get (ns-interns ns) sym)
+                          (str sym " is not public")
+                          (str sym " does not exist")))))
+          (. *ns* (refer (or (rename sym) sym) v)))))))
 
 (defn ns-refers
   "Returns a map of the refer mappings for the namespace."
