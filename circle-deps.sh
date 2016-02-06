6e1c540a754b71251c4d06df8c3f876e9e9eed48
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-02-05 21:27:46 -0600

    Manually download deps
    This patch creates a script, circle-deps.sh, which hashes itself, the
    pom and build files, and checks for a flag file in ~/.m2. If the flag is
    not found, then a full build is executed in such a way as to be
    effectively a dry run, that is to deploy no artifacts and soforth while
    using all plugins. This serves to fill the ~/.m2 cache. The flag file is
    then created and life continues.
    
    Future builds using the same pom and build files will not have to go
    through this double build process. Note that the <version/> tag is
    excluded from the hashed text, so that just changing the artifact
    version won't impact cache status.

---
+++ circle-deps.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR="~/.m2/tags"
+FILE="$DIR/$(cat pom.xml build.xml circle-deps.sh | grep -v "<version>.*</version>" | shasum -a 512 | awk '{print $1}')"
+mkdir -p "$DIR"
+if [ ! -f "$FILE" ]
+then
+  # Do a deploy
+  # - Forces a build
+  # - Forces but does not run tests
+  # - Uses all plugins
+  # - Without a profile, the deploy fails
+  mvn deploy -Dmaven.test.skip=true
+
+  # cleans up the mess
+  mvn clean
+
+  # purge clojarr from m2
+  # no reason to leave those lying about
+  rm -r ~/.m2/repository/me/arrdem/clojarr/
+
+  # leave the flag file behind
+  touch "$FILE"
+fi
