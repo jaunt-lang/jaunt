49bb5d817c7517be271416e8e8034a4c8ebe3913
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-03-06 16:22:00 -0600

    Don't look for changelog diff on develop/master

--- etc/bin/check-changelog.sh
+++ etc/bin/check-changelog.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-if ! git diff --name-only origin/develop HEAD | grep -ie changelog.md > /dev/null
-then
-  echo "No changelog entry in this patch!"
-  exit 1
-fi
+BRANCH=$(git name-rev --name-only HEAD)
+
+case $BRANCH in
+  master|develop|release*)
+    exit 0
+    ;;
+  
+  *)
+    if ! git diff --name-only origin/develop HEAD | grep -ie changelog.md > /dev/null
+    then
+      echo "No changelog entry in this patch!"
+      exit 1
+    fi
+    ;;
+esac
