ce5e82ba7d7235ff3dff8ceae880f16ec3d338a4
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-03-15 15:12:17 -0500

    Add a test runner script
    This script will do a clean and parallel build. If not on CircleCI, then
    parallelism is achieved by use of subshells. Otherwise the environment
    is expected to be providing container level parallelism.

---
+++ etc/bin/run-tests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function do_tests () {
+  case $CIRCLE_NODE_INDEX in
+    0)
+      ant test-example 2>&1 | tee test-example.log
+      ;;
+    1)
+      ant test-generative 2>&1 | tee test-generative.log
+      ;;
+  esac
+}
+
+mvn clean compile           # git info file, classfiles
+bash etc/bin/antsetup.sh    # set up standalone classpath
+
+if [ -n "$CIRCLE_NODE_INDEX" ]
+then
+  do_tests
+else
+  ( export CIRCLE_NODE_INDEX=0;
+    do_tests > /dev/null ) &
+  tex=$1
+  ( export CIRCLE_NODE_INDEX=1;
+    do_tests > /dev/null ) &
+  teg=$1
+  wait $tex
+  texr=$?
+  wait $teg
+  tegr=$?
+  if [ $texr ] && [ $tegr ]
+  then
+    echo "Tests OK!"
+  else
+    [ ! $texr ] && echo "Example tests failed, see test-example.log"
+    [ ! $tegr ] && echo "Generative tests failed, see test-generative.log"
+    echo "Tests failed :("
+    exit 1
+  fi
+fi
